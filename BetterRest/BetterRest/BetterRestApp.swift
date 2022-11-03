651248f4316e726b2f83fe6da447f229631684e5
Joshua Hales <joshua.hales@sky.uk>
2022-11-03 16:30:38 +0000

    Added the BetteRest project files

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Joshua Hales on 02/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
