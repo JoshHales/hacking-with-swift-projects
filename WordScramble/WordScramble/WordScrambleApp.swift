94f1df4c7e50ce3ed2431efad97b98df5962564e
Joshua Hales <joshua.hales@sky.uk>
2022-11-23 17:59:52 +0000

    Created project file

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Joshua Hales on 23/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
